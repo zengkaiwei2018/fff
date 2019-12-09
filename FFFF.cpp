31b0f141c74b68bc90d2bd844965ebe2bffe429b
zengkaiwei2018 <51750122+zengkaiwei2018@users.noreply.github.com>
2019-12-09 12:45:01 +0800

    Update FFFF.cpp

--- FFFF.cpp
+++ FFFF.cpp
@@ -1,22 +1,32 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include<string.h>
 int main()
 {
-	int A,B;
-	double y;
-	scanf("%d %d",&A,&B);
-	y=1.00*A/B;
-	if(B>0)
-	{
-	    printf("%d/%d=%.2f",A,B,y);
-	}
-	else if(B<0)
-	{
-	    printf("%d/(%d)=%.2f",A,B,y);
-	}
-	else if(B==0)
-	{
-	    printf("%d/%d=Error",A,B);
-	}
-
-	return 0;
+    char Stack[100];
+    int N, M;
+    scanf("%d %d", &N, &M);
+    for(int i=0; i<N; i++){
+        scanf("%s", Stack);
+        int l = 0, len = strlen(Stack), flag = 1;
+        for(int j=0; j<len; j++){
+            if(Stack[j]=='S')
+                l++;
+            else
+                l--;
+            if(l < 0 || l > M){
+                printf("NO\n");
+                flag = 0;
+                break;
+            }
+        }
+        if(flag == 1){
+            if(l == 0)
+                printf("YES\n", l);
+            else
+                printf("NO\n", l);
+        }
+ 
+    }
 }
